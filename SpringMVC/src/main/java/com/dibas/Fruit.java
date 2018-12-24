312743467c6b0314e09baa22ae2a9b48d4bb57a8
dibasbehera7 <dibasbehera@gmail.com>
2018-12-24 21:14:02 +0530

    Configuration Spring Beans from External Properties Files

--- SpringMVC/src/main/java/com/dibas/Fruit.java
+++ SpringMVC/src/main/java/com/dibas/Fruit.java
@@ -1,58 +1,55 @@
 package com.dibas;
 
-import java.util.List;
-import java.util.Map;
-
 public class Fruit {
 
-	private List<String> fruitNameList;
-	private Map<String, String> fruitNameMap;
+	private String definition = "not set";
 
-	public List<String> getFruitNameList() {
-		return fruitNameList;
+	public String getDefinition() {
+		return definition;
 	}
 
-	public void setFruitNameList(List<String> fruitNameList) {
-		this.fruitNameList = fruitNameList;
+	public void setDefinition(String definition) {
+		this.definition = definition;
 	}
 
-	public Map<String, String> getFruitNameMap() {
-		return fruitNameMap;
-	}
-
-	public void setFruitNameMap(Map<String, String> fruitNameMap) {
-		this.fruitNameMap = fruitNameMap;
-	}
+	/*
+	 * private List<String> fruitNameList; private Map<String, String> fruitNameMap;
+	 * 
+	 * public List<String> getFruitNameList() { return fruitNameList; }
+	 * 
+	 * public void setFruitNameList(List<String> fruitNameList) { this.fruitNameList
+	 * = fruitNameList; }
+	 * 
+	 * public Map<String, String> getFruitNameMap() { return fruitNameMap; }
+	 * 
+	 * public void setFruitNameMap(Map<String, String> fruitNameMap) {
+	 * this.fruitNameMap = fruitNameMap; }
+	 */
 
 	public Fruit() {
 		// no arg constructor
 	}
 
 	public String talkAboutYou() {
-
-		String speech = "Hi, I'm Fruit. ";
-
-		if (fruitNameList != null && fruitNameList.size() > 0) {
-			speech += "\nList :\n";
-			for (int i = 0; i < fruitNameList.size(); i++) {
-				speech += (i + 1) + " Name: " + fruitNameList.get(i) + "\n";
-			}
-		}
-
-		if (fruitNameMap != null && fruitNameMap.size() > 0) {
-
-			speech += "\nMap :\n";
-			// int i=0;
-			for (Map.Entry<String, String> fruitName : fruitNameMap.entrySet()) {
-				speech +=
-						// (i+1)+" "+
-						fruitName.getKey() + " --> " + fruitName.getValue() + "\n";
-				// i++;
-			}
-
-		}
-
-		return speech;
+		return definition;
 	}
+
+	/*
+	 * public String talkAboutYou() {
+	 * 
+	 * String speech = "Hi, I'm Fruit. ";
+	 * 
+	 * if (fruitNameList != null && fruitNameList.size() > 0) { speech +=
+	 * "\nList :\n"; for (int i = 0; i < fruitNameList.size(); i++) { speech += (i +
+	 * 1) + " Name: " + fruitNameList.get(i) + "\n"; } }
+	 * 
+	 * if (fruitNameMap != null && fruitNameMap.size() > 0) {
+	 * 
+	 * speech += "\nMap :\n"; // int i=0; for (Map.Entry<String, String> fruitName :
+	 * fruitNameMap.entrySet()) { speech += // (i+1)+" "+ fruitName.getKey() +
+	 * " --> " + fruitName.getValue() + "\n"; // i++; }
+	 * 
+	 * } return speech; }
+	 */
 
 }
--- SpringMVC/src/main/java/com/dibas/MyApp.java
+++ SpringMVC/src/main/java/com/dibas/MyApp.java
@@ -6,43 +6,44 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class MyApp {
 
 	public static void main(String[] args) {
-		
-		//Fruit myFruit = new Fruit();
-		//Vegetable myvegetable = new Vegetable();
-		
+
+		// Fruit myFruit = new Fruit();
+		// Vegetable myvegetable = new Vegetable();
+
 		@SuppressWarnings("resource")
 		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
-		Fruit myFruit = appContext.getBean("fruit",Fruit.class);
+		Fruit myFruit = appContext.getBean("fruit", Fruit.class);
 		Vegetable myvegetable = (Vegetable) appContext.getBean("vegetable");
-		
 		System.out.println(myFruit.talkAboutYou());
-		
 		System.out.println(myvegetable.talkAboutYou());
+
+		/*
+		 * Configuration Spring Beans from External Properties Files
+		 */
+		Fruit externalFruit = appContext.getBean("externalFruit",Fruit.class);
+		System.out.println("External Fruit:\n"+externalFruit.talkAboutYou());
 		
 		
 		/*
 		 * Setting Bean Properties Example
 		 */
-		Vegetable vegWithName = (Vegetable) appContext.getBean("vegWithName");
-		System.out.println(vegWithName.talkAboutYou());
-		
-		 /*
-		  * Setting Collection Type Bean Properties
-		  */
-		Fruit fruitWithListAndMap = (Fruit) appContext.getBean("fruitWithListAndMap");
-		System.out.println(fruitWithListAndMap.talkAboutYou());
-		
-		
+		// Vegetable vegWithName = (Vegetable) appContext.getBean("vegWithName");
+		// System.out.println(vegWithName.talkAboutYou());
 		/*
-		 * Setting Bean properties using  p-namespace
+		 * Setting Collection Type Bean Properties
 		 */
-		Vegetable vegUsingPNamespace = (Vegetable) appContext.getBean("vegUsingPNamespace");
-			System.out.println(vegUsingPNamespace.talkAboutYou());
-		
-		
-		((FileSystemXmlApplicationContext)appContext).close();
-			
+		// Fruit fruitWithListAndMap = (Fruit)
+		// appContext.getBean("fruitWithListAndMap");
+		// System.out.println(fruitWithListAndMap.talkAboutYou());
+		/*
+		 * Setting Bean properties using p-namespace
+		 */
+		// Vegetable vegUsingPNamespace = (Vegetable)
+		// appContext.getBean("vegUsingPNamespace");
+		// System.out.println(vegUsingPNamespace.talkAboutYou());
+
+		((FileSystemXmlApplicationContext) appContext).close();
+
 	}
 
-	
 }
